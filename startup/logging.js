3dd28489147a25ba07ce31c5e683f4e30ba9a635
Shahzaib Rind <shahzaibrind6@outlook.com>
2021-08-26 10:19:06 +0500

    Fully Tested

---
+++ startup/logging.js
@@ -0,0 +1,43 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+module.exports = function () {
+
+    winston.exceptions.handle(
+        new winston.transports.File({
+            filename: 'uncaughtExceptions.log'
+        }),
+    );
+
+    process.on('unhandledRejection', (ex) => {
+        // throwing back rejection log as an exception so that winston can catch that exception
+        throw ex;
+    });
+
+    winston.add(new winston.transports.File({
+        filename: 'logfile.log',
+        format: winston.format.combine(
+            winston.format.timestamp({
+                format: 'YYYY-MM-DD hh:mm:ss A ZZ'
+            }),
+            winston.format.json()
+        )
+    }));
+
+    winston.add(new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.colorize({ all: true }),
+            winston.format.prettyPrint(),
+            winston.format.simple()
+          ),
+          handleExceptions: true
+    }));
+
+    winston.add(new winston.transports.MongoDB({
+        db: 'mongodb://localhost/TranquilFortress',
+        options: {
+            useUnifiedTopology: true,
+        }
+    }));
+
+}
